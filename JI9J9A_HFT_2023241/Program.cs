ed52d7148cce66db753caf4329bd5df6dea3f1f7
Balint "Fish" Fuzi <54815531+Fish-dev2@users.noreply.github.com>
2023-10-23 19:03:04 +0200

    Add project files.

---
+++ JI9J9A_HFT_2023241/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JI9J9A_HFT_2023241
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
